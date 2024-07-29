35ca184df535d5ff7b5d5b3fe8114fd0207e159a
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-07-29 23:57:57 +0530

    created user schema

---
+++ backend/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    role: {type:String, enum:['admin','editor','viewer'], default:'viewer'}
+
+});
+
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) return next();
+    const salt = await bcrypt.hash(this.password, salt);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+mongoose.exports = User;
