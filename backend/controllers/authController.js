f65600e5f997ca61bb618004641606675301c64f
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2024-07-31 00:29:21 +0530

    created Auth Controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,28 @@
+const User = require('../models/User');
+const {generateToken} = require('../utils/jwtUtils');
+const bcrypt = require('bcrypt');
+
+exports.registerUser = async (req, res) => {
+    const {username, email, password} = req.body;
+    try {
+        const user = new User({username, email, password});
+        await user.save();
+        res.status(201).json({message:'User Registered Successfully'});
+    } catch (error) {
+        res.status(400).json({message: 'Error registering user', error: error.message});
+    };
+};
+
+exports.loginUser = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await User.findOne({email});
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({message: 'Invalid email or password'});
+        }
+        const token = generateToken(user._id);
+        res.json({token});
+    }catch(error){
+        res.status(400).json({error: error.message});
+    }
+}
